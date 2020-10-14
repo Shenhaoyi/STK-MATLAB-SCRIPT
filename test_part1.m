89047566a6f99edbc69830ea1a0734b17af1cdf1
沈浩奕 <7877185+shenhaoyi@user.noreply.gitee.com>
2020-10-14 14:31:24 +0800

    基本完成

--- test_part1.m
+++ test_part1.m
@@ -1,14 +1,18 @@
 %% 获取句柄，创建场景
 app = actxserver('STK11.application');
+% 不知何用
+app.UserControl = 1;
+app.visible = 1;
+
 root = app.Personality2;
-root.NewScenario('Astro');
+root.NewScenario('Astro'); % 创建并命名
 scenario = root.CurrentScenario;
 
 %% 场景初始化时间
 scenario.StartTime = '1 Jan 2000 00:00:00.000';
 
 %% 创建卫星
-satellite = scenario.Children.New(18,'mysat'); 
+satellite = scenario.Children.New(18,'mysat'); % 创建并命名
 % satellite = scenario.Children.New('eSatellite','ValidationSat');
 % 与18的区别是什么？
 
@@ -31,7 +35,7 @@ end
 
 %% 发动机推力幅值和比冲设置
 customThruster.Isp = 3000;
-customThruster.Thrust = 1/2000;
+customThruster.Thrust = 1;
 
 %% 新建初始段
 initstate = MCS.Insert('eVASegmentTypeInitialState','initstate','-');
@@ -41,7 +45,7 @@ initstate.SetElementType('eVAElementTypeKeplerian');     %经典轨道根数
 
 %% 初始质量设置
 initstate.InitialState.DryMass = 1000; %除了燃料之外的质量
-initstate.FuelTank.FuelMass = 2000;
+initstate.FuelTank.FuelMass = 1000;
 
 %% 气动板、光电版等参数设置
 initstate.InitialState.Cd = 2.2;
@@ -50,52 +54,66 @@ initstate.InitialState.Cr = 1;
 initstate.InitialState.SRPArea = 20;
 
 %% 初始轨道根数设置
-initstate.InitialState.Element.SemiMajorAxis = 7.8781e+03;
-initstate.InitialState.Element.Inclination = 50;
+initstate.Element.SemiMajorAxis = 7.8781e+03;
+initstate.Element.Inclination = 50;
 
 %% 初始轨道外推段
-propagate = MCS.Insert('eVASegmentTypePropagate','Propagate','-');
+propagate = MCS.Insert('eVASegmentTypePropagate','Propagate','-');%三个参数分别是：模块类型、自定义的模块名称、后一个模块的名称（在该模块前插入新的模块）
 propagate.PropagatorName = 'Earth Point Mass';
 propagate.Properties.Color = uint32(hex2dec('00ff00'));
 propagate.StoppingConditions.Item('Duration').Properties.Trip = 3600;
 
+
+%% 修改成只运行更新段
+satellite.Propagator.Option.SmartRunMode = 1; %'eVASmartRunModeOnlyChanged'
+
+%% 循环插入
+max = 100;
+count = 1;
+res = [];
+while count <= max
+    name = strcat('Man',num2str(count));
+    
+    man = MCS.Insert('eVASegmentTypeManeuver',name,'-');
+    man.Properties.Color = uint32(hex2dec('0000ff'));
+    man.SetManeuverType('eVAManeuverTypeFinite'); % 设置为有限推力
+    man.InitialState.SetElementType('eVAElementTypeKeplerian');
+    man.FinalState.SetElementType('eVAElementTypeKeplerian');
+    maneuver = man.Maneuver;
+    maneuver.SetPropulsionMethod('eVAPropulsionMethodEngineModel','customThruster');% 选择发动机为复制的发动机
+%     maneuver.Propagator.PropagatorName = 'Earth Point Mass'; %动力学模型
+    maneuver.Propagator.PropagatorName = 'Earth J2'; %动力学模型
+    maneuver.ThrustEfficiencyMode = 'eVAThrustTypeAffectsAccelAndMassFlow';%设置质量变化
+    maneuver.SetAttitudeControlType('eVAAttitudeControlThrustVector');%姿态控制设置
+    maneuver.AttitudeControl.ThrustAxesName = 'Satellite LVLH'; %推力所在的坐标系设置
+    maneuver.AttitudeControl.ThrustVector.AssignXYZ(0,1,0);
+    maneuver.Propagator.StoppingConditions.Item('Duration').Properties.Trip = 3600*24;
+    satellite.Propagator.RunMCS;
+    % 获取该段推进之后的轨道根数
+    res = [res;man.FinalState.Element.SemiMajorAxis];
+    count = count + 1;
+end
+%% 新建机动段
+% man = MCS.Insert('eVASegmentTypeManeuver','Man','-');
+% man.Properties.Color = uint32(hex2dec('0000ff'));
+% man.SetManeuverType('eVAManeuverTypeFinite'); % 设置为有限推力
+% man.InitialState.SetElementType('eVAElementTypeKeplerian');
+% man.FinalState.SetElementType('eVAElementTypeKeplerian');
+% maneuver = man.Maneuver;
+% maneuver.SetPropulsionMethod('eVAPropulsionMethodEngineModel','customThruster');% 选择发动机为复制的发动机
+% maneuver.Propagator.PropagatorName = 'Earth Point Mass'; %动力学模型
+% maneuver.ThrustEfficiencyMode = 'eVAThrustTypeAffectsAccelAndMassFlow';%设置质量变化
+% maneuver.SetAttitudeControlType('eVAAttitudeControlThrustVector');%姿态控制设置
+% maneuver.AttitudeControl.ThrustAxesName = 'Satellite LVLH'; %推力所在的坐标系设置
+% maneuver.AttitudeControl.ThrustVector.AssignXYZ(1,1,1);
+% maneuver.Propagator.StoppingConditions.Item('Duration').Properties.Trip = 3600*240;
+
 %% 目标轨道外推段
-propagate2 = MCS.Insert('eVASegmentTypePropagate','Propagate','-');
+propagate2 = MCS.Insert('eVASegmentTypePropagate','Propagate2','-');
 propagate2.PropagatorName = 'Earth Point Mass';
 propagate2.Properties.Color = uint32(hex2dec('00ff00'));
-propagate2.StoppingConditions.Item('Duration').Properties.Trip = 5184000;
+propagate2.StoppingConditions.Item('Duration').Properties.Trip = 3600;
 
 %% 运行
 satellite.Propagator.RunMCS;
-
-%%
-%先插入Target Sequence模块
-mytars = MCS.Insert('eVASegmentTypeTargetSequence','mytarget','-');
-%在Target Sequence模块前插入初始状态模块
-myinit = MCS.Insert('eVASegmentTypeInitialState','myinit','mytarget');
-%修改初始时刻
-myinit.InitialState.Epoch = '1 Jan 2000 00:00:00.000';
-%从上面语句，就可以看出Insert方法的使用方式，三个参数分别是：模块类型、自定义的模块名称、后一个模块的名称（在该模块前插入新的模块）
-% pro500 = MCS.Insert('eVASegmentTypePropagate','pro500','mytarget');
-myinit.InitialState.get
-
-myinit.InitialState.SetElementType('eVAElementTypeKeplerian');
-%设置轨道高度为500km，没设置的会有默认值
-myinit.InitialState.Element.SemiMajorAxis = 6.8781e+03;
-myinit.InitialState.Element.Inclination = 50;
-%初始质量
-sat.Propagator.MainSequence.Item(0).InitialState.DryMass =1000;
-
-%设置pro500模块运行500s
-%Propagate模块默认的停止条件是运行时间，这里我们就不修改了
-% pro500.StoppingConditions.Item(0).Properties.Trip = 500;
-
-man1 = mytars.Segments.Insert('eVASegmentTypeManeuver','man1','-');
-%设置该段初始时刻和结束时刻的轨道参数类型
-man1.InitialState.SetElementType('eVAElementTypeKeplerian')
-man1.FinalState.SetElementType('eVAElementTypeKeplerian')
-man1.Maneuver.get
-man1.SetManeuverType('eVAManeuverTypeFinite');%有限推力
-man1.Maneuver.ThrustEfficiencyMode = 'eVAThrustTypeAffectsAccelAndMassFlow';%设置质量变化
-
-%首先需要自己先在stk中新建一个engine
+% 如何run only changed segments
--- test_part2.m
+++ test_part2.m
@@ -14,36 +14,7 @@ man1.Maneuver.SetAttitudeControlType('eVAAttitudeControlThrustVector');%姿态控制
 man1.Maneuver.AttitudeControl.ThrustAxesName = 'Satellite LVLH'; %推力设置的坐标系
 man1.Maneuver.Propagator.StoppingConditions.Item('Duration').Properties.Trip = 600;
 
-%% Astrogator Config
-componentAstrogator = scenario.ComponentDirectory.GetComponents('eComponentAstrogator');
-engineModels = componentAstrogator.GetFolder('Engine Models');
-try
-    customThruster = engineModels.Item('customThruster');
-catch
-    customThruster = engineModels.DuplicateComponent('Constant Thrust and Isp','customThruster');
-end
-customThruster.Isp = 720;
-customThruster.Thrust = 0.001;
 
-man2 = mytars.Segments.Insert('eVASegmentTypeManeuver','man2','-');
-man2.Properties.Color = uint32(hex2dec('00d3ff'));
-man2.InitialState.SetElementType('eVAElementTypeKeplerian')
-man2.FinalState.SetElementType('eVAElementTypeKeplerian')
-man2.SetManeuverType('eVAManeuverTypeFinite');
-man2.Maneuver.ThrustEfficiencyMode = 'eVAThrustTypeAffectsAccelAndMassFlow';%设置质量变化
-
-maneuver = man2.Maneuver;
-maneuver.SetAttitudeControlType('eVAAttitudeControlThrustVector');
-maneuver.AttitudeControl.ThrustAxesName = 'Satellite VNC(Earth)';
-maneuver.AttitudeControl.ThrustVector.AssignXYZ(1,0,0);
-maneuver.Propagator.StoppingConditions.Item('Duration').Properties.Trip = 864000;
-maneuver.Propagator.PropagatorName = 'Earth Point Mass';
-maneuver.SetPropulsionMethod('eVAPropulsionMethodEngineModel','customThruster');
-
-propagate2 = MCS.Insert('eVASegmentTypePropagate','Propagate','-');
-propagate2.PropagatorName = 'Earth Point Mass';
-propagate2.Properties.Color = uint32(hex2dec('00ff00'));
-propagate2.StoppingConditions.Item('Duration').Properties.Trip = 3600;
 
 
 %如何只运行一段？
